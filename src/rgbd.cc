194a94949ce3251f089accfc81a850165ce8d7cd
ahmadi86 <h.ahmadi86@hotmail.com>
2018-02-01 13:05:48 -0800

    odom publishing

--- src/rgbd.cc
+++ src/rgbd.cc
@@ -31,6 +31,7 @@
 #include <message_filters/sync_policies/approximate_time.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <nav_msgs/Odometry.h>
 
 #include <opencv2/core/core.hpp>
 
@@ -42,16 +43,33 @@ class ImageGrabber
 {
 public:
     typedef ORB_SLAM2::Tracking::eTrackingState eTrackingState;
-    ImageGrabber(ORB_SLAM2::System* pSLAM);
+    ImageGrabber(ORB_SLAM2::System* pSLAM, ros::NodeHandle nh);
 
     void GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD);
+    /**
+     *
+     * @brief
+     */
+    tf::StampedTransform broadcastTfs(cv::Mat T_cam_world);
 
+    /**
+     *
+     * @brief 
+     */
+    void publishNavMsg(tf::StampedTransform current_world_cam);
+
+protected:
     ORB_SLAM2::System* mpSLAM;
+
+    ros::NodeHandle mNodeHandler;
+    ros::Publisher mOdomPublisher;
     
     tf::TransformBroadcaster mtfBroadcaster;
     tf::TransformListener mtfListener;
     /**! Transformation used to setup the transform of ORBSLAM, for better visualization **/
     tf::Transform mTransform_odom_world;
+
+    tf::StampedTransform mStampedTransform_world_cam;
     
     std::string mStrWorldFrameId;
     std::string mStrCameraFrameId;
@@ -63,6 +81,11 @@ public:
     std::string mStrCameraOpticalFrameId;
 
     eTrackingState mLastTrackingState;
+
+    double mnPosePositionVariance;
+    double mnPoseOrientationVariance;
+    double mnTwistPositionVariance;
+    double mnTwistOrientationVariance;
 };
 
 int main(int argc, char **argv)
@@ -80,9 +103,8 @@ int main(int argc, char **argv)
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
 
-    ImageGrabber igb(&SLAM);
-
     ros::NodeHandle nh("~");
+    ImageGrabber igb(&SLAM, nh);
 
     message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, "/camera/rgb/image_raw", 1);
     message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "/camera/depth_registered/image_raw", 1);
@@ -103,7 +125,8 @@ int main(int argc, char **argv)
     return 0;
 }
  
-ImageGrabber::ImageGrabber(ORB_SLAM2::System* pSLAM) : mpSLAM(pSLAM), mLastTrackingState(eTrackingState::SYSTEM_NOT_READY)
+ImageGrabber::ImageGrabber(ORB_SLAM2::System* pSLAM, ros::NodeHandle nh) : 
+    mpSLAM(pSLAM), mNodeHandler(nh), mLastTrackingState(eTrackingState::SYSTEM_NOT_READY)
 {
     ros::param::param<std::string>("~world_frame",      mStrWorldFrameId,       "orb_world");
     ros::param::param<std::string>("~camera_frame",     mStrCameraFrameId,      "orb_camera");
@@ -112,6 +135,13 @@ ImageGrabber::ImageGrabber(ORB_SLAM2::System* pSLAM) : mpSLAM(pSLAM), mLastTrack
 
     ros::param::param<std::string>("~cameralink_frame",         mStrCameraLinkId,           "camera_link");
     ros::param::param<std::string>("~cameraopticallink_frame",  mStrCameraOpticalFrameId,   "camera_rgb_optical_frame");
+
+    ros::param::param<double>("~pose_position_variance",        mnPosePositionVariance,     9999.0);
+    ros::param::param<double>("~pose_orientation_variance",     mnPoseOrientationVariance,  9999.0);
+    ros::param::param<double>("~twist_position_variance",       mnPosePositionVariance,     9999999.0);
+    ros::param::param<double>("~twist_orientation_variance",    mnPoseOrientationVariance,  9999999.0);
+
+    mOdomPublisher = mNodeHandler.advertise<nav_msgs::Odometry>("visual_odom", 10);
 
     mTransform_odom_world.setIdentity();
 }
@@ -142,7 +172,22 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
     }
 
     cv::Mat T_cam_world = mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec());
+    auto stampedTransform_world_cam = broadcastTfs(T_cam_world);
+    
+    auto currentTrackingState = (eTrackingState)mpSLAM->GetTrackingState();
+    
+    // publish the odom topic
+    if (mLastTrackingState == eTrackingState::OK && currentTrackingState == eTrackingState::OK)
+    {
+        publishNavMsg(stampedTransform_world_cam);
+    }
 
+    mStampedTransform_world_cam = stampedTransform_world_cam;
+    mLastTrackingState = currentTrackingState;
+}
+
+tf::StampedTransform ImageGrabber::broadcastTfs(cv::Mat T_cam_world)
+{
     tf::Transform transform_cam_world;
     if (!T_cam_world.empty())
     {
@@ -192,15 +237,80 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
         mTransform_odom_world = transform_odom_base * transfrom_base_cam * transform_cam_world;
     }
 
-    mtfBroadcaster.sendTransform(tf::StampedTransform(
+    tf::StampedTransform stampedTransform_world_cam = tf::StampedTransform(
         transform_cam_world.inverse(), ros::Time::now(), mStrWorldFrameId, mStrCameraFrameId
-    ));
+    );
+    mtfBroadcaster.sendTransform(stampedTransform_world_cam);
 
     mtfBroadcaster.sendTransform(tf::StampedTransform(
         mTransform_odom_world, ros::Time::now(), mStrOdomFrameId, mStrWorldFrameId
     ));
 
-    mLastTrackingState = currentTrackingState;
+    return stampedTransform_world_cam;
+}
+
+void ImageGrabber::publishNavMsg(tf::StampedTransform stampedTransform_world_cam)
+{
+    nav_msgs::Odometry odom_msg;
+    odom_msg.header.stamp = ros::Time::now();
+    odom_msg.header.frame_id = mStrWorldFrameId;
+    odom_msg.child_frame_id = mStrCameraFrameId;
+    
+    auto position = stampedTransform_world_cam.getOrigin();
+    auto orientation = stampedTransform_world_cam.getRotation();
+    odom_msg.pose.pose.position.x = position.getX();
+    odom_msg.pose.pose.position.y = position.getY();
+    odom_msg.pose.pose.position.z = position.getZ(); 
+
+    odom_msg.pose.pose.orientation.x = orientation.getX();
+    odom_msg.pose.pose.orientation.y = orientation.getY();
+    odom_msg.pose.pose.orientation.z = orientation.getZ();
+    odom_msg.pose.pose.orientation.w = orientation.getW();
+
+    // TODO: covariances
+
+    // relative transform between last and current
+    double dt = stampedTransform_world_cam.stamp_.toSec() - mStampedTransform_world_cam.stamp_.toSec();
+
+    tf::Transform relative_transform = mStampedTransform_world_cam.inverse() * stampedTransform_world_cam;
+    auto linear_velocity = relative_transform.getOrigin()/dt;
+    
+    geometry_msgs::Vector3 angular_velocity;
+    tf::Matrix3x3(relative_transform.getRotation()).getRPY(
+        angular_velocity.x,
+        angular_velocity.y,
+        angular_velocity.z
+    );
+    angular_velocity.x /= dt;
+    angular_velocity.y /= dt;
+    angular_velocity.z /= dt;
+
+    odom_msg.twist.twist.linear.x = linear_velocity.getX();
+    odom_msg.twist.twist.linear.y = linear_velocity.getY();
+    odom_msg.twist.twist.linear.z = linear_velocity.getZ();
+
+    odom_msg.twist.twist.angular.x = angular_velocity.x;
+    odom_msg.twist.twist.angular.y = angular_velocity.y;
+    odom_msg.twist.twist.angular.z = angular_velocity.z;
+
+    // covariances
+    odom_msg.pose.covariance[0]     = mnPosePositionVariance;
+    odom_msg.pose.covariance[7]     = mnPosePositionVariance;
+    odom_msg.pose.covariance[14]    = mnPosePositionVariance;
+
+    odom_msg.pose.covariance[21]    = mnPoseOrientationVariance;
+    odom_msg.pose.covariance[28]    = mnPoseOrientationVariance;
+    odom_msg.pose.covariance[35]    = mnPoseOrientationVariance;
+
+    odom_msg.twist.covariance[0]    = mnTwistPositionVariance;
+    odom_msg.twist.covariance[7]    = mnTwistPositionVariance;
+    odom_msg.twist.covariance[14]   = mnTwistPositionVariance;
+
+    odom_msg.twist.covariance[21]   = mnTwistOrientationVariance;
+    odom_msg.twist.covariance[28]   = mnTwistOrientationVariance;
+    odom_msg.twist.covariance[35]   = mnTwistOrientationVariance;
+
+    mOdomPublisher.publish(odom_msg);
 }
 
 
